45d55ee03e35eed1ab4b0594a3bdc97b4e5dddae
Geethkiran <geeth53xy@gmail.com>
2020-04-29 10:19:41 +0530

    [geeth]Add. greeting repository interface is defined to provide loose coupling

---
+++ src/main/java/com/myfirstapp/repository/GreetingRepository.java
@@ -0,0 +1,6 @@
+package com.myfirstapp.repository;
+
+import com.myfirstapp.model.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface GreetingRepository extends JpaRepository<Greeting, Long>{
+}
